47e064b7314ef9b73fe0d46f34696127ff896a1a
milan <send2milankumar@gmail.com>
2019-11-09 01:59:12 +0530

    fraction and integer number both can be converted

--- Decimal_to_Binary_conversion.cpp
+++ Decimal_to_Binary_conversion.cpp
@@ -1,23 +1,29 @@
-// Decimal to binary using Recursion
-
 #include <iostream>
 #include <cmath>
 using namespace std;
-
-void decimalToBinary(int n){
+void f(int n){
 	if(n==1) cout<<1;
 	else{
 		
-		DecimalToBinary(ceil(n/2));
+		f(ceil(n/2));
 		cout<< n%2;
 	}
 }
 int main() {
-	// your code goes here
-	int n;
+	// for integer part to binary
+	double n;
 	cin>>n;
 	f(n);
+	double d; int i;
+	i=n; //integer part
+	d=n-i;//decimal part
+	cout<<".";
+	// below part is for converting decimal fraction
+	while(true){//for finding upto precision k ; run loop k times only
+		i=d*2;
+		cout<<i;
+		d=d*2-i;
+		if(d==0) break;
+	}
 	return 0;
 }
-Chat conversation end
-Type a message...
