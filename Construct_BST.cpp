8a25d02426b1c6c920b5778b76469896c33708c7
milan <send2milankumar@gmail.com>
2019-11-12 19:47:16 +0530

    added function to search tree for a value

--- Construct_BST.cpp
+++ Construct_BST.cpp
@@ -24,9 +24,24 @@ void inOrder(Node* root)
         inOrder(root->right); 
     } 
 } 
+int minValue(Node *tree){
+    Node *current= new Node();
+    current=tree;
+    while(current->left!=NULL ){
+        current=current->left;
+    }
+    return current->data;
+}
+bool ifNodeExists(Node *tree,int key){
+    if(tree==NULL) return false;
+    if(tree->data==key) return true;
+    bool recleft=ifNodeExists(tree->left,key);
+    bool recright=ifNodeExists(tree->right,key);
+    return recleft || recright;
+}
 
 int main()
-{int arr[] = { 100, 20, 3, 4, 5, 61, 2, 63, 64 };
+{int arr[] = { 100, 20, 3, 4, 5, 61, 20, 63, 64 };
 int n = sizeof(arr)/sizeof(arr[0]);
    Node* tree =new Node();
    for(int i=0;i<n;i++){
@@ -34,5 +49,8 @@ int n = sizeof(arr)/sizeof(arr[0]);
     insert(arr[i],tree);   
    }
    inOrder(tree);
+   cout<<"tree printed..."<<endl;
+   cout<<"min value is "<<minValue(tree)<<endl;
+   cout<<"Node exists or not (0 / 1 ): "<<ifNodeExists(tree,33);
    return 0;
 }
